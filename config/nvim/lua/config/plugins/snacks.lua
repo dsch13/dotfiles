161ed570b661b2556f6a13f993e86e67fa48620f
Drew Schmit <drewschmit@gmail.com>
2025-05-01 08:51:30 -0500

    update smooth scroll on jump

--- config/nvim/lua/config/plugins/snacks.lua
+++ config/nvim/lua/config/plugins/snacks.lua
@@ -1,42 +1,61 @@
 return {
-    "folke/snacks.nvim",
-    priority = 1000,
-    lazy = false,
-    opts = {
-        bigfile = { enabled = true },
-        dashboard = require("config.plugins.snacks.dashboard").dashboard,
-        indent = { enabled = true },
-        input = { enabled = true },
-        notifier = {
-            enabled = true,
-        },
-        quickfile = { enabled = true },
-        words = { enabled = true },
-    },
-    keys = {
-        { "<leader>n",  function() require("snacks").notifier.show_history() end, desc = "[N]otification history" },
-        { "<leader>db", function() require("snacks").bufdelete() end,             desc = "[D]elete [B]uffer" }
-    },
-    init = function()
-        vim.api.nvim_create_autocmd("User", {
-            pattern = "VeryLazy",
-            callback = function()
-                -- Setup some globals for debugging (lazy-loaded)
-                _G.dd = function(...)
-                    Snacks.debug.inspect(...)
-                end
-                _G.bt = function()
-                    Snacks.debug.backtrace()
-                end
-                print = function(...)
-                    local print_safe_args = {}
-                    local _ = { ... }
-                    for i = 1, #_ do
-                        table.insert(print_safe_args, tostring(_[i]))
-                    end
-                    Snacks.notifier.notify(table.concat(print_safe_args, " "), "info")
-                end
-            end,
-        })
-    end
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	opts = {
+		bigfile = { enabled = true },
+		dashboard = require("config.plugins.snacks.dashboard").dashboard,
+		indent = { enabled = true },
+		input = { enabled = true },
+		notifier = {
+			enabled = true,
+		},
+		quickfile = { enabled = true },
+		words = { enabled = true },
+		---@class snacks.scroll.Config
+		scroll = {
+			enabled = true,
+			animate = {
+				duration = { step = 45, total = 100 },
+			},
+		},
+	},
+	keys = {
+		{
+			"<leader>n",
+			function()
+				require("snacks").notifier.show_history()
+			end,
+			desc = "[N]otification history",
+		},
+		{
+			"<leader>db",
+			function()
+				require("snacks").bufdelete()
+			end,
+			desc = "[D]elete [B]uffer",
+		},
+	},
+	init = function()
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "VeryLazy",
+			callback = function()
+				-- Setup some globals for debugging (lazy-loaded)
+				_G.dd = function(...)
+					Snacks.debug.inspect(...)
+				end
+				_G.bt = function()
+					Snacks.debug.backtrace()
+				end
+				print = function(...)
+					local print_safe_args = {}
+					local _ = { ... }
+					for i = 1, #_ do
+						table.insert(print_safe_args, tostring(_[i]))
+					end
+					Snacks.notifier.notify(table.concat(print_safe_args, " "), "info")
+				end
+			end,
+		})
+	end,
 }
