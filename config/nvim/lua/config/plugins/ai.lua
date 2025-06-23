31d8b50b6e1a82d5e9da31bc2cb7fb0b404da321
Drew Schmit <drewschmit@gmail.com>
2025-06-22 22:01:04 -0500

    implement mcphub

--- config/nvim/lua/config/plugins/ai.lua
+++ config/nvim/lua/config/plugins/ai.lua
@@ -2,16 +2,6 @@ return {
 	"yetone/avante.nvim",
 	event = "VeryLazy",
 	version = false,
-	opts = {
-		provider = "copilot",
-		auto_suggestions_provider = nil,
-		behavior = {
-			use_cwd_as_project_root = true,
-		},
-		file_selector = {
-			provider = "telescope",
-		},
-	},
 	build = "make",
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter",
@@ -27,5 +17,40 @@ return {
 			},
 			ft = { "markdown", "Avante" },
 		},
+		{
+			"ravitemer/mcphub.nvim",
+			dependencies = {
+				"nvim-lua/plenary.nvim",
+			},
+			build = "npm install -g mcp-hub@latest",
+			config = function()
+				require("mcphub").setup()
+			end,
+		},
+	},
+	opts = {
+		provider = "copilot",
+		providers = {
+			copilot = {
+				disabled_tools = { "web_search" },
+			},
+		},
+		auto_suggestions_provider = nil,
+		behavior = {
+			use_cwd_as_project_root = true,
+			enable_cursor_planning_mode = true,
+		},
+		file_selector = {
+			provider = "telescope",
+		},
+		system_prompt = function()
+			local hub = require("mcphub").get_hub_instance()
+			return hub and hub:get_active_servers_prompt() or ""
+		end,
+		custom_tools = function()
+			return {
+				require("mcphub.extensions.avante").mcp_tool(),
+			}
+		end,
 	},
 }
